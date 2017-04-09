6396ea010998bbad4c2838559f3be079e85e59ab
James Chant <jamesrchant@gmail.com>
2017-04-09 11:43:12 +0100

    Added dictionary in loop so data is neatly stored inside a master list

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ browser.get("http://store.steampowered.com/tag/en/Indie/")
 html_source = browser.page_source
 soup = bs.BeautifulSoup(html_source, 'lxml')
 
+
 # Defining the initial source page
 body = soup.body
 
@@ -34,33 +35,27 @@ for links in body.find_all('a', href=True):
 
 # De-dupe the list and discard those that don't refer to game pages
 de_dupe(indie_game_list)
-print(indie_game_list[3:])
 
-# Delete after testing
-indie_game_list = ['http://store.steampowered.com/app/341110/?snr=1_237_237__103',
-                   'http://store.steampowered.com/app/611350/?snr=1_237_237__103',
-                   'http://store.steampowered.com/app/538510/?snr=1_237_237__103',
-                   'http://store.steampowered.com/app/621840/?snr=1_237_237__103',
-                   'http://store.steampowered.com/app/238320/?snr=1_237_237__104',
-                   'http://store.steampowered.com/app/588920/?snr=1_237_237__103',
-                   'http://store.steampowered.com/app/342180/?snr=1_237_237__106']
+
+# Create a new empty list, which will store dictionaries of our data
+game_titles_and_desc = []
+
 
 # Follow those links, minus the first three to Steam hardware, and collect the data for them
 for target in indie_game_list[3:]:
     browser.get(str(target))
-    print(str(target))
-    # Using Selenium to click the Continue button on age check if triggered
+    # Create a new dictionary for each target
+    game_dict = {'url': str(target)}
+    # Using Selenium to handle the age check button / form if triggered
     game_page = browser.page_source
     if 'agecheck' in browser.current_url:
         btn = browser.find_elements_by_link_text("Continue")
         form = browser.find_elements_by_id("agecheck_form")
         if len(btn) > 0 and btn[0].is_displayed():
-            print("button")
             browser.implicitly_wait(5)
             btn[0].click()
             game_page = browser.page_source
         elif len(form) > 0 and form[0].is_displayed():
-            print("form")
             select_day = Select(browser.find_element_by_name('ageDay'))
             select_day.select_by_value('10')
             select_month = Select(browser.find_element_by_name('ageMonth'))
@@ -74,19 +69,24 @@ for target in indie_game_list[3:]:
         else:
             print("Age Check barrier")
             continue
+    # Now we have the actual game page, get the title and description
     soup = bs.BeautifulSoup(game_page, 'lxml')
     body = soup.body
     game_title = soup.find("div", {"class" : "apphub_AppName"})
     game_desc = soup.find("div", {"class" : "game_description_snippet"})
+    # Where they exist, add them to the instance disctionary
     if game_title:
-        print(game_title.string)
+        game_dict['title'] = game_title.text.replace("\n", "").replace("\t", "")
     else:
         print("No Title")
     if game_desc:
-        print(game_desc.string)
+        game_dict['desc'] = game_desc.text.replace("\n", "").replace("\t", "")
     else:
         print("No Description")
+    # Finally add the target's dictionary of data to the master list
+    game_titles_and_desc.append(game_dict)
 
 browser.quit()
 
-# Create a list where each value is a dictionary of title and description values
+# Show all the data we've gathered
+print(game_titles_and_desc)
