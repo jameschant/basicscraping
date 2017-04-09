1f93f8c87c07b3fec1d78ae7d285134d48aa2b6a
James Chant <jamesrchant@gmail.com>
2017-04-09 10:12:36 +0100

    Age check button click working

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome(executable_path=r"/home/james/beautifulsoup/chromedriver")
 browser.get("http://store.steampowered.com/tag/en/Indie/")
@@ -36,11 +36,18 @@ de_dupe(indie_game_list)
 print(indie_game_list[3:])
 
 
-# Follow those links and collect the data for them
+# Follow those links, minus the first three to Steam hardware, and collect the data for them
 for target in indie_game_list[3:]:
-    # browser = webdriver.Chrome(executable_path=r"/home/james/beautifulsoup/chromedriver")
     browser.get(str(target))
+    print(str(target))
+    # I need to use Selenium to click that Continue button on age check if triggered
     game_page = browser.page_source
+    if 'agecheck' in browser.current_url:
+        print('Continue')
+        btn = browser.find_element_by_link_text("Continue")
+        browser.implicitly_wait(5)
+        btn.click()
+        game_page = browser.page_source
     soup = bs.BeautifulSoup(game_page, 'lxml')
     body = soup.body
     game_title = soup.find("div", {"class" : "apphub_AppName"})
@@ -50,4 +57,4 @@ for target in indie_game_list[3:]:
 
 browser.quit()
 
-    # Create a list where each value is a dictionary of title and description values
+# Create a list where each value is a dictionary of title and description values
